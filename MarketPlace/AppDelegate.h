284644edbfa2e31a79ad6e178269126a83cbf690
brianhph <brianhph@yahoo-inc.com>
2015-07-11 15:12:17 +0800

    Initial Commit

---
+++ MarketPlace/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MarketPlace
+//
+//  Created by Brian Huang on 7/11/15.
+//  Copyright (c) 2015 EC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MarketPlace/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MarketPlace
+//
+//  Created by Brian Huang on 7/11/15.
+//  Copyright (c) 2015 EC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
